c008206b8456e6909141ef1d524b0b8515d8ae44
nicolamarino <nicolamarino527@gmail.com>
2025-02-13 16:20:48 +0100

    setup body

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 import Header from './components/header.jsx'
+import Main from './components/Main.jsx'
+import Footer from './components/Footer.jsx'
+
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
-  );
+  )
 }
 
 export default App
+
+
+
+
+
